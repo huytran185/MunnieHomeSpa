e26fd097f0ecb59f68dce3f6acc6813393021ce0
tranm <huytran18594@gmail.com>
2021-03-04 21:50:58 +1100

    Adding Admin Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Index from './pages/Index/Index';
 import Service from './pages/Service/Service';
 import Contact from './pages/Contact/Contact';
 import Error from './pages/Error/Error';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/home" exact component={Index}/>
           <Route path="/service" exact component={Service}/>
           <Route path="/contact" exact component={Contact}/>
+          <Route path="/login" exact component={Login}/>
           <Route path="/facebook" exact render={()=>{window.location.href ="https://www.facebook.com"; return null;}}/>
           <Route path="/instagram" exact render={()=>{window.location.href ="https://www.instagram.com"; return null;}}/>
           <Route  path='*' exact={true} component={Error}/>
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Login.module.css';
+import Auth from '../Auth/Auth'
+const Login = ()=>{
+    return(
+        <div>
+            <Auth/>
+        </div>
+    )
+}
+
+export default Login;
